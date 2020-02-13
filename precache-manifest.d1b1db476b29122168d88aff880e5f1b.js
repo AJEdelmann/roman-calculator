e12c39fa5e6835e53d72a980b2895363b9420bca
ajedelmann <albert.edelmann@gmail.com>
2020-02-13 22:39:29 +0100

    Updates

---
+++ precache-manifest.d1b1db476b29122168d88aff880e5f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d3893c5f99cf68fda388587fb49c32",
+    "url": "/roman-calculator/index.html"
+  },
+  {
+    "revision": "4f92d08c465684e99d22",
+    "url": "/roman-calculator/static/css/main.9d905a01.chunk.css"
+  },
+  {
+    "revision": "2153987b499ef5285241",
+    "url": "/roman-calculator/static/js/2.56d180ca.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/roman-calculator/static/js/2.56d180ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f92d08c465684e99d22",
+    "url": "/roman-calculator/static/js/main.b72608f2.chunk.js"
+  },
+  {
+    "revision": "9dee922e5ba4899e770b",
+    "url": "/roman-calculator/static/js/runtime-main.cc3949b9.js"
+  }
+]);
